de1d2dbaa51d3e87e4ccbd2992d15d6de7dde2ec
Trisna Chandra Laksana <109122013+Trisnavhi@users.noreply.github.com>
2023-06-23 22:18:55 +0700

    first commit

--- src/Hourglass.js
+++ src/Hourglass.js
@@ -1,34 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './hourglass.css';
 
 const Hourglass = () => {
-  const [countdown, setCountdown] = useState(900); // Durasi total countdown (15 menit = 900 detik)
-
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setCountdown((prevCountdown) => prevCountdown - 1);
-    }, 1000); // Mengurangi countdown setiap 1 detik
-
-    return () => {
-      clearInterval(timer);
-    };
-  }, []);
-
-  const formatTime = (seconds) => {
-    const minutes = Math.floor(seconds / 60);
-    const remainingSeconds = seconds % 60;
-
-    const formattedMinutes = String(minutes).padStart(2, '0');
-    const formattedSeconds = String(remainingSeconds).padStart(2, '0');
-
-    return `${formattedMinutes}:${formattedSeconds}`;
-  };
-
-  return (
-    <div>
-      <div className="hourglass"></div>
-    </div>
-  );
+  return <div className="hourglass"></div>;
 };
 
 export default Hourglass;
